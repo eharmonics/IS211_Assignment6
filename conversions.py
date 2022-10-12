d1f6f6cb35ec4f475915057d5745a050f80bbfdb
eharmonics <99229935+eharmonics@users.noreply.github.com>
2022-10-12 17:39:45 -0500

    Update conversions.py

--- conversions.py
+++ conversions.py
@@ -1,26 +1,18 @@
-def j():
-    def Cel_To_Fah(n):
-        m = (n * 9 / 5) + 32
-        return m
+def convertCelsiusToKelvin(celsius):
+    return celsius + 273.15
 
+def convertCelsiusToFahrnite(celsius):
+    return 1.8*celsius+32
 
-    def Celsius_to_Kelvin(C):
-        return (C + 273.15)
+def  convertFahrenheittoCelsius(farhnit):
+    return float(5/9)*(farhnit-32)
 
-    inp = input('enter 1 Cel_To_Fah enter 2 Celsius_to_Kelvin')
-    if (inp == 1				):
-        m = input('enter Celsius')
-        n=float(m)
+def convertFahrenheitToKelvin(farhnit):
+    return float(5/9)*(farhnit+459.67)
+# Kelvinto Fahrenheit and Celsius
+def KelvinToFahrenheit(kelvin):
+    return (kelvin-273.15)*float(9/5)+32
 
-        print(Cel_To_Fah(n))
-    else:
-        m= input('enter Celsius')
-        C=float(m)
-        print("Temperature in Kelvin ( K ) = ",
-              Celsius_to_Kelvin(C))
-
-
-j()
-j()
-
+def KelvinToCelsius(kelvin):
+    return kelvin-273.15
 
