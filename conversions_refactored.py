126588809f0eaea4233674780b50f348f84d7595
eharmonics <99229935+eharmonics@users.noreply.github.com>
2022-10-17 09:57:25 -0500

    Update conversions_refactored.py

--- conversions_refactored.py
+++ conversions_refactored.py
@@ -1,30 +1,69 @@
-def convert(fromUnit,toUnit,val):
-    if(fromUnit=="celsius" and toUnit=="fahrenheit"):
-        return 1.8*val+32
-    elif fromUnit=="celsius" and toUnit=="kelvin":
-        return val + 273.15
-    elif toUnit=="celsius" and fromUnit=="fahrenheit":
-        return float(5/9)*(val-32)
-    elif toUnit=="kelvin" and fromUnit=="fahrenheit":
-        return float(5/9)*(val+459.67)
-    elif fromUnit=="kelvin" and toUnit=="fahrenheit":
-       return (val-273.15)*float(9/5)+32
-    elif fromUnit=="kelvin" and toUnit=="celsius":
-        return val - 273.15
+from conversions import *
 
-    #distance conversion
-    elif fromUnit=="mile" and toUnit=="yard":
-        return val*1760 
-    elif fromUnit=="yard" and toUnit=="mile":
-        return val/1760
-    elif fromUnit=="mile" and toUnit=="meter":
-        return val*1609.34
-    elif fromUnit=="meter" and toUnit=="mile":
-        return val/1609.34
-    elif fromUnit=="meter" and toUnit=="yard":
-        return val*1.094
-    elif fromUnit=="yard" and toUnit=="meter":
-        return val/1.094
-    return Exception
+class ConversionNotPossible(Exception):
+
+    def __init__(self, message):
+        self.message = message
+
+        def __str__(self):
+            return self.message
+
+
+def convert(from_units, to_units, value):
+
+    from_lcase = from_units.lower()
+    to_lcase = to_units.lower()
+
+    if from_lcase == 'celsius' and to_lcase == "kelvin":
+        return convertCelsiusToKelvin(value)
+    elif from_lcase == 'celsius' and to_lcase == "fahrenheit":
+        return convertCelsiusToFahrenheit(value)
+    elif from_lcase == "miles" and to_lcase == "meters":
+        return convertMilesToMeters(value)
+    elif from_lcase == "miles" and to_lcase == "yards":
+        return convertMilesToYards(value)
+    elif from_lcase == "yards" and to_lcase == "miles":
+        return convertYardsToMiles(value)
+    elif from_lcase == "yards" and to_lcase == "meters":
+        return convertYardsToMeters(value)
+    elif from_lcase == "meters" and to_lcase == "miles":
+        return convertMetersToMiles(value)
+    elif from_lcase == "meters" and to_lcase == "yards":
+        return convertMetersToYards(value)
+    elif from_lcase == "fahrenheit" and to_lcase == "celsius":
+        return convertFahrenheitToCelsius(value)
+    elif from_lcase == "fahrenheit" and to_lcase == "kelvin":
+        return convertFahrenheitToKelvin(value)
+    elif from_lcase == "kelvin" and to_lcase == "celsius":
+        return convertKelvinToCelsius(value)
+    elif from_lcase == "kelvin" and to_lcase == "fahrenheit":
+        return convertKelvintoFahrenheit(value)
+    
+    elif from_lcase == "miles" and to_lcase == "fahrenheit":
+        raise ConversionNotPossible("Cannot convert miles to fahrenheit")
+    elif from_lcase == "miles" and to_lcase == "kelvin":
+        raise ConversionNotPossible("Cannot convert miles to kelvin")
+    elif from_lcase == "yards" and to_lcase == "fahrenheit":
+        raise ConversionNotPossible("Cannot convert yards to fahrenheit")
+    elif from_lcase == "yards" and to_lcase == "kelvin":
+        raise ConversionNotPossible("Cannot convert yards to kelvin")
+    elif from_lcase == "meters" and to_lcase == "fahrenheit":
+        raise ConversionNotPossible("Cannot convert meters to fahrenheit")
+    elif from_lcase == "meters" and to_lcase == "kelvin":
+        raise ConversionNotPossible("Cannot convert meters to kelvin")
+    elif from_lcase == "fahrenheit" and to_lcase == "miles":
+        raise ConversionNotPossible("Cannot convert fahrenheit to miles")
+    elif from_lcase == "fahrenheit" and to_lcase == "yards":
+        raise ConversionNotPossible("Cannot convert fahrenheit to yards")
+    elif from_lcase == "fahrenheit" and to_lcase == "meters":
+        raise ConversionNotPossible("Cannot convert fahrenheit to meters")
+    elif from_lcase == "kelvin" and to_lcase == "miles":
+        raise ConversionNotPossible("Cannot convert kelvin to miles")
+    elif from_lcase == "kelvin" and to_lcase == "yards":
+        raise ConversionNotPossible("Cannot convert kelvin to yards")
+    elif from_lcase == "kelvin" and to_lcase == "meters":
+        raise ConversionNotPossible("Cannot convert kelvin to meters")
+    else:
+        return None
 
 
